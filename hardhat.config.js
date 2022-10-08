42d2daadaa1ca02eb4fcd7ace46cf5c6f0d1306d
haycarlitos <carlos.castilloperera@gmail.com>
2022-10-08 18:57:43 -0500

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     mumbai: {
-      url: "<https://rpc-mumbai.maticvigil.com>",
-      accounts: process.env.PRIVATE_KEY,
+      url: "https://rpc-mumbai.maticvigil.com/",
+      accounts: [process.env.PRIVATE_KEY,],
     },
   },
   paths: {
